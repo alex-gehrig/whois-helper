318d3ac03887641d7707c8a4dabbdd8716ae08d3
Alexander Gehrig <alexander.gehrig@gmx.de>
2018-04-25 18:49:18 +0200

    fixed an error in the code

--- whois-helper.py
+++ whois-helper.py
@@ -5,7 +5,12 @@ import sys
 import subprocess
 import time
 
-# the file which contains ip-addresses and number of occurance must be
+"""Takes a textfile with ip-adresses and their frequency as input, performs
+whois-request using the Linux-Bash and produces a csv-output showing the
+ip-address and the correspondign frequency, the countrycode and the owner
+of the ip-address"""
+
+# the file which contains the ip-addresses and number of occurance must be
 # specified when calling the script. if not, there is a little error-
 # message and the script terminates
 try:
@@ -14,6 +19,7 @@ except:
     print("""
     Error! You must provide a file as input like this:
     python3 whois-helper.py addresses.txt
+    The script will be terminated now.
     """)
     sys.exit()
 
@@ -30,7 +36,9 @@ outputfile.write("\"frequency\",\"IP-address\",\"countrycode\",\"description\"\n
 with open(inputfile) as source:
     for line in source:
         line = line.strip()
-        # split the number of occurance and ip-address
+        cc_tmp_list = []
+        desc_tmp_list = []
+        # split the frequency and ip-address
         pieces = line.split(' ')
         # the command to execute
         command = subprocess.Popen(['whois', pieces[1]], stdout = subprocess.PIPE)
@@ -38,38 +46,35 @@ with open(inputfile) as source:
         print("whois-query for:", pieces[1])
 
         # write the number of occurance and the ip-address in the file for the results
-        outputfile.write("\"" + str(pieces[0]) + "\"," + "\"" + str(pieces[1]) + "\",")
+        outputfile.write("\"{}\",\"{}\",".format(pieces[0], pieces[1]))
 
         # looping through the result of the whois-query
         for line in command.stdout:
-            tmp_list = []
             line = line.strip().decode('UTF-8')
 
             if line.startswith("country:") or line.startswith("Country:"):
-                pieces = line.strip().split(":")
-                countrycode = pieces[1].strip()
+                parts = line.strip().split(":")
+                cc_tmp_list.append(parts[1].strip())
 
             if line.startswith("descr:") or line.startswith("OrgName") or line.startswith("owner:"):
-                pieces = line.strip().split(":")
-                tmp_list.append(pieces[1].strip())
-                # usually there is more than one line matching the current
-                # pattern, but I want only the first one
-                description = tmp_list[0]
+                parts = line.strip().split(":")
+                desc_tmp_list.append(parts[1].strip())
+
+        # usually there is more than one line matching the current
+        # pattern, but I want only the first ones
+        countrycode = cc_tmp_list[0]
+        description = desc_tmp_list[0]
 
         # write countrycode and description to the file for the results
-        outputfile.write("\"" + str(countrycode) + "\",")
-        outputfile.write("\"" + str(description) + "\"")
+        outputfile.write("\"{}\",\"{}\"\n".format(countrycode, description))
 
         # Setting the variables to "None" again, in case an incorrect ip-address is queried
         # otherwise the data of the previous ip-address would be written again
         countrycode = None
         description = None
 
-        # adding the line break
-        outputfile.write("\n")
-
-        # wait for 5 seconds in order not to get blocked
-        time.sleep(2)
+        # wait for 3 seconds in order not to get blocked - hopefully
+        time.sleep(3)
 
 # close the file for the results
 outputfile.close()
